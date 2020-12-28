6acf6455fb5bb3fadf3ae7814f3de6dcb2ab7ff6
Maik Freitas <maikemir2003@gmail.com>
2020-12-28 16:00:58 -0300

    Primeiro commit do Curso

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Exemplos.Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
